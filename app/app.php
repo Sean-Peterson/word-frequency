ebd6d6e4b3adc65c853c1bbd92cf2205a60d6430
Sean Peterson <seanpeterson11@gmail.com>
2017-02-17 11:45:44 -0800

    link pages together and properly display results

--- app/app.php
+++ app/app.php
@@ -6,19 +6,19 @@
 
     $app = new Silex\Application();
 
-    session_start();
-    if (empty($_SESSION['chess'])) {
-      $_SESSION['chess'] = array();
-    }
-
-
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         "twig.path" => __DIR__.'/../views'
     ));
     $app['debug'] = true;
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig', array('something'=>$something));
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/results", function() use ($app) {
+        $new_word_frequency = new WordFrequency($_POST['string'], $_POST['word']);
+        $result = $new_word_frequency->wordFrequency();
+        return $app['twig']->render('results.html.twig', array('result'=>$result));
     });
 
 
