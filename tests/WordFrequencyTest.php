d0f8cb378b4ed2b0f6a2b8fc2c115d461205add0
Sean Peterson <seanpeterson11@gmail.com>
2017-02-17 09:27:13 -0800

    add passing second test

--- tests/WordFrequencyTest.php
+++ tests/WordFrequencyTest.php
@@ -20,4 +20,20 @@
             $this->assertEquals(1, $result);
 
         }
+
+        function test_WordFrequency_checkforMoreThanOneWord()
+        {
+            //Arrange
+            $user_input_one = "the cat walked the elephant and the other cat";
+            $user_input_two = "cat";
+            $test_word_frequency = new WordFrequency($user_input_one, $user_input_two);
+
+
+            //Act
+            $result = $test_word_frequency->wordFrequency();
+
+            //Assert
+            $this->assertEquals(2, $result);
+
+        }
     }
